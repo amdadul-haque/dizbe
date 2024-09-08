32b7a876019f1d26f9cf981665018dd19d81ff70
Amdadul Haque <amdadul1807107@gmail.com>
2024-09-08 20:45:59 +0600

    hero

--- components/hero.js
+++ components/hero.js
@@ -11,7 +11,7 @@ const Hero = () => {
             <h1 className='text-3xl md:text-4xl lg:text-5xl xl:text-[52px] font-bold leading-none'>Precision in Every Cart</h1>
             <p className='text-base md:text-lg w-[90%] leading-[1.1] mt-3 md:mt-5'>Dizzbe revolutionizes your shopping experience with AI-driven, contextually aware search. Whether you’re at home or on the go, our platform adapts to your needs—suggesting products based on your location, weather, and events. Discover the future of shopping with unparalleled precision, wherever you are.</p>
             <div>
-              <Button buttonText='Get Started' buttonLink='/signup' className='mt-8 md:mt-10' />
+              <Button buttonText='Get Started' buttonLink='/' className='mt-8 md:mt-10' />
             </div>
           </div>
           <div className='w-full flex justify-center md:justify-end '>
